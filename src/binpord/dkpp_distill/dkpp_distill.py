6ecd66a5f822411611567268dde57c1e557e5d78
Shiyanov Vadim <vadimsh853@gmail.com>
2022-04-19 20:52:31 +0200

    Add DKPP and TALS experiments

---
+++ src/binpord/dkpp_distill/dkpp_distill.py
@@ -0,0 +1,122 @@
+import os
+import sys
+from typing import Optional
+
+import torch
+from torchdistill.losses.single import KDLoss
+
+# For imports from common
+# TODO: is there a better way?
+sys.path.append(os.path.abspath(os.path.join(__file__, "../../..")))
+
+from common import (  # noqa: E402
+    Accuracy,
+    AdamWOneCycleLRMixin,
+    CIFAR100_Mixin,
+    Experiment,
+    ModelCheckpointMixin,
+    resnet18,
+)
+
+
+class DKPPLoss:
+    """
+    Dark Knowledge with Permuted Predictions loss as described in Born Again Networks paper (arXiv:1805.04770).
+
+    The idea here is to prove that the dark knowledge doesn't play any role in KD success via permuting the
+    non-true classes predictions in a random fashion.
+    Unfortunately the paper doesn't give many details on the subject, so I had to come up with specifics myself.
+    As such this class has two modes: persistent and non-persistent permutations, and I experiment with both.
+    """
+
+    def __init__(self, temperature, persistent_permutation=True):
+        self.kd_loss = KDLoss(temperature, alpha=0)
+        self.persistent_permutation = persistent_permutation
+        self.permutation = None
+
+    def __call__(self, student_output: torch.Tensor, teacher_output: torch.Tensor, targets: torch.Tensor):
+        batch_size, n_classes = student_output.size()
+        if self.persistent_permutation:
+            if self.permutation is None:
+                self.permutation = torch.randperm(n_classes, device=student_output.device)
+
+            permutation = self.permutation[None, :].repeat(batch_size, 1)
+        else:
+            permutation = torch.stack(
+                [torch.randperm(student_output.size(1), device=student_output.device) for _ in range(batch_size)],
+                dim=0,
+            )
+
+        # Here, permutation may permute the target class. I need to fix that.
+        self._fix_permutation(permutation, targets)
+
+        teacher_output = self._apply_permutation(teacher_output, permutation)
+        return self.kd_loss(student_output, teacher_output)
+
+    @staticmethod
+    def _fix_permutation(permutation: torch.Tensor, targets: torch.Tensor):
+        batch_size = targets.size(0)
+        _, indices = (permutation == targets[:, None]).nonzero(as_tuple=True)
+        permuted_targets_indices = permutation[range(batch_size), targets]
+        permutation[range(batch_size), targets] = targets
+        permutation[range(batch_size), indices] = permuted_targets_indices
+
+    @staticmethod
+    def _apply_permutation(teacher_output: torch.Tensor, permutation: torch.Tensor) -> torch.Tensor:
+        return torch.gather(teacher_output, dim=1, index=permutation)
+
+
+class DKPPDistillExperiment(Experiment, CIFAR100_Mixin, AdamWOneCycleLRMixin, ModelCheckpointMixin):
+    def __init__(
+        self,
+        experiment_name: str = "dkpp_distill",
+        wandb_project: str = "kd-cifar100-resnet18",
+        artifacts_base: str = "./artifacts",
+        temperature: float = 4.0,
+        persistent_permutation: bool = True,
+        teacher_checkpoint: Optional[str] = None,
+        batch_size: int = 1024,
+        epochs: int = 50,
+        lr: float = 1e-3,
+        weight_decay: float = 0.01,
+    ):
+        super().__init__()
+        self.save_hyperparameters()
+
+        self.student_model = resnet18()
+        self.teacher_model = resnet18(checkpoint_path=teacher_checkpoint, freeze=True)
+        self.criterion = DKPPLoss(temperature, persistent_permutation)
+
+        self.train_acc = Accuracy()
+        self.val_acc = Accuracy()
+
+    def forward(self, x):
+        return self.student_model(x)
+
+    def training_step(self, batch, batch_idx):
+        image, target = batch
+        student_output = self.student_model(image)
+        teacher_output = self.teacher_model(image)
+
+        loss = self.criterion(student_output, teacher_output, target)
+        self.log("loss", loss)
+        self.log("train_loss", loss, on_step=False, on_epoch=True)
+
+        self.train_acc(student_output, target)
+        self.log("train_acc", self.train_acc, on_step=False, on_epoch=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        image, target = batch
+        student_output = self.student_model(image)
+        teacher_output = self.teacher_model(image)
+
+        loss = self.criterion(student_output, teacher_output, target)
+        self.log("val_loss", loss)
+
+        self.val_acc(student_output, target)
+        self.log("val_acc", self.val_acc)
+
+
+if __name__ == "__main__":
+    DKPPDistillExperiment.main()
---
+++ src/binpord/tals_distill/tals_distill.py
@@ -0,0 +1,94 @@
+import os
+import sys
+from typing import Optional
+
+import torch
+
+# For imports from common
+# TODO: is there a better way?
+sys.path.append(os.path.abspath(os.path.join(__file__, "../../..")))
+
+from common import (  # noqa: E402
+    Accuracy,
+    AdamWOneCycleLRMixin,
+    CIFAR100_Mixin,
+    Experiment,
+    ModelCheckpointMixin,
+    resnet18,
+)
+
+
+class TALSLoss:
+    """
+    Teacher Aware Label Smoothing loss.
+
+    The idea here is to interpret the knowledge distillation as a label smoothing technique.
+    Namely, we can try and use label smoothing with the target probability given by the teacher model.
+    """
+
+    @staticmethod
+    def __call__(student_output: torch.Tensor, teacher_output: torch.Tensor, targets: torch.Tensor):
+        batch_size, n_classes = student_output.size()
+        teacher_probs = torch.softmax(teacher_output, dim=1)
+        teacher_probs = teacher_probs[range(batch_size), targets]
+        smooth_probs = (1 - teacher_probs) / (n_classes - 1)
+
+        target_probs = smooth_probs[:, None].repeat(1, n_classes)
+        target_probs[range(batch_size), targets] = teacher_probs
+
+        student_logprobs = torch.log_softmax(student_output, dim=1)
+        return -(target_probs * student_logprobs).sum(dim=1).mean()
+
+
+class TALSDistillExperiment(Experiment, CIFAR100_Mixin, AdamWOneCycleLRMixin, ModelCheckpointMixin):
+    def __init__(
+        self,
+        experiment_name: str = "tals_distill",
+        wandb_project: str = "kd-cifar100-resnet18",
+        artifacts_base: str = "./artifacts",
+        teacher_checkpoint: Optional[str] = None,
+        batch_size: int = 1024,
+        epochs: int = 50,
+        lr: float = 1e-3,
+        weight_decay: float = 0.01,
+    ):
+        super().__init__()
+        self.save_hyperparameters()
+
+        self.student_model = resnet18()
+        self.teacher_model = resnet18(checkpoint_path=teacher_checkpoint, freeze=True)
+        self.criterion = TALSLoss()
+
+        self.train_acc = Accuracy()
+        self.val_acc = Accuracy()
+
+    def forward(self, x):
+        return self.student_model(x)
+
+    def training_step(self, batch, batch_idx):
+        image, target = batch
+        student_output = self.student_model(image)
+        teacher_output = self.teacher_model(image)
+
+        loss = self.criterion(student_output, teacher_output, target)
+        self.log("loss", loss)
+        self.log("train_loss", loss, on_step=False, on_epoch=True)
+
+        self.train_acc(student_output, target)
+        self.log("train_acc", self.train_acc, on_step=False, on_epoch=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        image, target = batch
+        student_output = self.student_model(image)
+        teacher_output = self.teacher_model(image)
+
+        loss = self.criterion(student_output, teacher_output, target)
+        self.log("val_loss", loss)
+
+        self.val_acc(student_output, target)
+        self.log("val_acc", self.val_acc)
+
+
+if __name__ == "__main__":
+    TALSDistillExperiment.main()
